4fb1770c0a78a93bf339c0c6eed0e21926a9b8ad
chuka <emry390@gmail.com>
2023-10-16 12:37:30 +0100

    Day 31: Add url endpoint for a year and all data.

--- main.py
+++ main.py
@@ -23,5 +23,25 @@ def about(station, date):
             "Temperature": temperature}
 
 
+@app.route('/api/v1/yearly/<station>/<year>')
+def annual(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    # Series.str can be used to access the values of the series as strings and apply several methods to it.
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    print(result)
+    return result
+
+
+@app.route('/api/v1/<station>')
+def station(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # attribute "orient=" changes how the dict is structured.
+    result = df.to_dict(orient="records")
+    return result
+
+
 if __name__ == '__main__':
     app.run(debug=True)
